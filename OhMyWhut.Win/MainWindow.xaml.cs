f3504c54bfcf012d28893199644953937f452662
saicem <saicem@qq.com>
2022-09-26 19:21:45 +0800

    feat: custom navigation

--- OhMyWhut.Win/MainWindow.xaml.cs
+++ OhMyWhut.Win/MainWindow.xaml.cs
@@ -3,13 +3,13 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using OhMyWhut.Engine;
 using OhMyWhut.Win.Controls;
 using OhMyWhut.Win.Data;
 using OhMyWhut.Win.Pages;
 using OhMyWhut.Win.Services;
-using Windows.Foundation.Metadata;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,8 +23,18 @@ namespace OhMyWhut.Win
     {
         private readonly FrameNavigationOptions navOptions;
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits", Justification = "<挂起>")]
         public MainWindow()
+        {
+            PrepareData();
+            InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(TitleBar);
+
+            navOptions = new FrameNavigationOptions();
+            navOptions.IsNavigationStackEnabled = false;
+        }
+
+        private void PrepareData()
         {
             using (var scope = App.Current.Services.CreateScope())
             {
@@ -39,7 +49,7 @@ namespace OhMyWhut.Win
                 else
                 {
                     gluttony.SetUserInfo(appPreference.UserName, appPreference.Password);
-                    new Task(() =>
+                    _ = Task.Run(() =>
                     {
                         using (var scope = App.Current.Services.CreateScope())
                         {
@@ -47,117 +57,56 @@ namespace OhMyWhut.Win
                             fetcher.UpdateCoursesAsync().Wait();
                             fetcher.UpdateBooksAsync().Wait();
                         }
-                    }).Start();
+                    });
                 }
                 if (appPreference.MeterId != string.Empty)
                 {
-                    new Task(() =>
+                    _ = Task.Run(() =>
                     {
                         using (var scope = App.Current.Services.CreateScope())
                         {
                             var fetcher = scope.ServiceProvider.GetService<DataFetcher>();
                             fetcher.UpdateElectricFeeAsync().Wait();
                         }
-                    }).Start();
+                    });
                 }
             }
-
-            InitializeComponent();
-            ExtendsContentIntoTitleBar = true;
-            SetTitleBar(AppTitleBar);
-
-            navOptions = new FrameNavigationOptions();
-            navOptions.IsNavigationStackEnabled = false;
-            NavigationViewControl.SelectedItem = HomeNavItem;
         }
 
         public string AppTitleText
         {
             get
             {
-#if !UNIVERSAL && DEBUG
+#if DEBUG
                 return "OhMyWhut Dev";
-#elif !UNIVERSAL
-                return "OhMyWhut";
-#elif DEBUG
-                return "OhMyWhut (UWP)";
 #else
-                return "OhMyWhut (UWP)";
+                return "OhMyWhut";
 #endif
             }
         }
 
-        private void OnNavViewItemSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private void NavigationTap(object sender, TappedRoutedEventArgs e)
         {
-            if (args.IsSettingsSelected)
+            var name = (sender as Button).Name;
+            if (name == "HomeButton")
             {
-                rootFrame.NavigateToType(typeof(ConfigPage), null, navOptions);
+                RootFrame.NavigateToType(typeof(HomePage), null, navOptions);
+            }
+            else if (name == "CourseButton")
+            {
+                RootFrame.NavigateToType(typeof(CoursePage), null, navOptions);
+            }
+            else if (name == "BookButton")
+            {
+                RootFrame.NavigateToType(typeof(BookPage), null, navOptions);
+            }
+            else if (name == "ElectricFeeButton")
+            {
+                RootFrame.NavigateToType(typeof(ElectricPage), null, navOptions);
             }
             else
             {
-                var selectedTag = (args.SelectedItem as NavigationViewItem).Tag.ToString();
-                string pageName = "OhMyWhut.Win.Pages." + selectedTag;
-                rootFrame.NavigateToType(Type.GetType(pageName), null, navOptions);
-            }
-        }
-
-        private void NavigationViewControl_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
-        {
-            Thickness currMargin = AppTitleBar.Margin;
-            if (sender.DisplayMode == NavigationViewDisplayMode.Minimal)
-            {
-                AppTitleBar.Margin = new Thickness() { Left = (sender.CompactPaneLength * 2), Top = currMargin.Top, Right = currMargin.Right, Bottom = currMargin.Bottom };
-
-            }
-            else
-            {
-                AppTitleBar.Margin = new Thickness() { Left = sender.CompactPaneLength, Top = currMargin.Top, Right = currMargin.Right, Bottom = currMargin.Bottom };
-            }
-
-            UpdateAppTitleMargin(sender);
-        }
-
-        private void NavigationViewControl_PaneClosing(NavigationView sender, NavigationViewPaneClosingEventArgs args)
-        {
-            UpdateAppTitleMargin(sender);
-        }
-
-        private void NavigationViewControl_PaneOpening(NavigationView sender, object args)
-        {
-            UpdateAppTitleMargin(sender);
-        }
-
-        private void UpdateAppTitleMargin(NavigationView sender)
-        {
-            const int smallLeftIndent = 4, largeLeftIndent = 24;
-
-            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7))
-            {
-                AppTitle.TranslationTransition = new Vector3Transition();
-
-                if ((sender.DisplayMode == NavigationViewDisplayMode.Expanded && sender.IsPaneOpen) ||
-                         sender.DisplayMode == NavigationViewDisplayMode.Minimal)
-                {
-                    AppTitle.Translation = new System.Numerics.Vector3(smallLeftIndent, 0, 0);
-                }
-                else
-                {
-                    AppTitle.Translation = new System.Numerics.Vector3(largeLeftIndent, 0, 0);
-                }
-            }
-            else
-            {
-                Thickness currMargin = AppTitle.Margin;
-
-                if ((sender.DisplayMode == NavigationViewDisplayMode.Expanded && sender.IsPaneOpen) ||
-                         sender.DisplayMode == NavigationViewDisplayMode.Minimal)
-                {
-                    AppTitle.Margin = new Thickness() { Left = smallLeftIndent, Top = currMargin.Top, Right = currMargin.Right, Bottom = currMargin.Bottom };
-                }
-                else
-                {
-                    AppTitle.Margin = new Thickness() { Left = largeLeftIndent, Top = currMargin.Top, Right = currMargin.Right, Bottom = currMargin.Bottom };
-                }
+                throw new Exception("未处理的导航选项");
             }
         }
     }
