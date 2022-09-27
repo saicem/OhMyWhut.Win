ca2e251288e3471e606e1d2da8a3701b9907fa7b
saicem <saicem@qq.com>
2022-09-27 10:28:16 +0800

    refactor: app preference only save can write property

--- OhMyWhut.Win/Services/AppPreference.cs
+++ OhMyWhut.Win/Services/AppPreference.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using OhMyWhut.Win.Data;
@@ -7,6 +8,10 @@ namespace OhMyWhut.Win.Services
 {
     public class AppPreference
     {
+        public bool IsSetUserInfo { get => UserName != string.Empty; }
+        
+        public bool IsSetMeterInfo { get => MeterId != string.Empty; }
+
         public string UserName { get; set; } = string.Empty;
 
         public string Password { get; set; } = string.Empty;
@@ -36,9 +41,9 @@ namespace OhMyWhut.Win.Services
 
         public async Task SaveAsync(AppDbContext db)
         {
-            var items = GetType().GetProperties();
+            var items = GetType().GetProperties().Where(p => p.CanWrite).ToArray();
             var entities = new Preference[items.Length];
-            for (int i = 0; i < entities.Length; i++)
+            for (int i = 0; i < items.Length; i++)
             {
                 entities[i] = new Preference
                 {
